919fe3208fb879a5c9bc591a7779b03e99fa06e8
Derick Tsai (蔡信邦) <derick@C-MBP-C02T84AAFVH4.local>
2023-03-01 23:28:53 +0800

    新增練習題檔案

--- week2/tutorial_conditions.py
+++ week2/tutorial_conditions.py
@@ -1,15 +1,31 @@
-# score to grade
-score = input('請輸入成績: ')
-score = int(score)
-
-if score >= 90:
-    print('A')
-elif score >= 80:
-    print('B')
-elif score >= 70:
-    print('C')
-elif score >= 60:
-    print('D')
+# # 判斷式
+# # 假設某個程式回傳的結果應等於35
+result = 35
+if result == 35:
+    print("結果正確")
 else:
-    print('E')
+    print("結果錯誤")
+
+# 需注意型別，不同型別無法比較
+number_str = "100"
+number = 100
+
+if number_str == number:
+    print("數值一樣！")
+
+
+# # score to grade
+# score = input('請輸入成績: ')
+# score = int(score)
+
+# if score >= 90:
+#     print('A')
+# elif score >= 80:
+#     print('B')
+# elif score >= 70:
+#     print('C')
+# elif score >= 60:
+#     print('D')
+# else:
+#     print('E')
 
