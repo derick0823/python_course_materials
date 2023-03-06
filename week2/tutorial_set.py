b5f17fabf52de0e77758180f385b9c4299738007
derick <s.b.tsai0823@gmail.com>
2023-03-07 02:38:43 +0800

    訂正tutorial_set.py

--- week2/tutorial_set.py
+++ week2/tutorial_set.py
@@ -1,6 +1,15 @@
+# create
 my_set = { 3,3,3,3,4,5 }
 print(my_set)
 
-# print( 7 in my_set )
+# read
+print( 7 in my_set )
 
-# del my_set[3]
+# delete
+my_set.remove(3)
+print(my_set)
+
+# update 只能移除後新增
+my_set.remove(4)
+my_set.add(8)
+print(my_set)
